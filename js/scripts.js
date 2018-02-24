8ff6cb83591f38a83fd4c53193990686f9c8e01e
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-02-24 03:08:01 +0300

    add the the business logic and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+function ping(input){
+  for (var i = 1; i <=input; i++) {
+    if ((i%3===0) && (i%5===0)){
+
+     $(".output").append("<li>"+ "pingpong"+"</li>");
+
+   }else if (i%3===0) {
+
+     $(".output").append("<li>"+"ping"+"</li>");
+   } else if (i%5===0) {
+
+     $(".output").append("<li>"+"pong"+"</li>");
+   }
+   else{
+
+      $(".output").append("<li>"+i+"</li>");
+    }
+  }//end of for
+}
+//call the function
+
 $(document).ready(function(){
   //gather user input
   //var input=parseInt(prompt("Enter the number"));
@@ -8,4 +29,7 @@ $(document).ready(function(){
 
     event.preventDefault();
   });
+
+
+
 });
